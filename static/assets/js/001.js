7d836c921fc3e797b519255dab96b5effbe7bc2f
ISAMEMAIO <joooommm@outlook.com>
2025-05-18 15:38:04 -0700

    Update 001.js

--- static/assets/js/001.js
+++ static/assets/js/001.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     const html = `
       <div id="icon-container">
-        <a class="icon" href="/./"><img alt="nav" id="INImg" src="${LogoUrl}"/></a>
+        <div class="icon"><img alt="nav" id="INImg" src="${LogoUrl}"/></div>
       </div>
       <div class="f-nav-right">
         <a class="navbar-link" href="/./a"><i class="fa-solid fa-gamepad navbar-icon"></i><an>&#71;&#97;</an><an>&#109;&#101;&#115;</an></a>
