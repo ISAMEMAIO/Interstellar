bf0629cc62979985ac1054d2837a63805100229f
ISAMEMAIO <joooommm@outlook.com>
2025-06-23 14:38:01 -0700

    Update 001.js

--- static/assets/js/001.js
+++ static/assets/js/001.js
@@ -33,9 +33,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (nav) {
     const themeId = localStorage.getItem("theme");
-    let LogoUrl = "/assets/media/favicon/Airbrush-Image-Enhancer-1750712843935-removebg-preview.png";
+    let LogoUrl = "/assets/media/favicon/.png";
     if (themeId === "Inverted") {
-      LogoUrl = "/assets/media/favicon/main-inverted.png";
+      LogoUrl = "/assets/media/favicon/atom.png";
     }
     const html = `
       <div id="icon-container">
