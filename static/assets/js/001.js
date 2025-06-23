302995d9b4a1488c85da4a1f801648d980e7dbac
ISAMEMAIO <joooommm@outlook.com>
2025-06-23 15:56:37 -0700

    Update 001.js

--- static/assets/js/001.js
+++ static/assets/js/001.js
@@ -33,7 +33,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   if (nav) {
     const themeId = localStorage.getItem("theme");
-    let LogoUrl = "/assets/media/favicon/atom3.jpeg";
+    let LogoUrl = "/assets/media/favicon/Atom.jpeg";
     if (themeId === "Inverted") {
       LogoUrl = "/assets/media/favicon/.jpg";
     }
